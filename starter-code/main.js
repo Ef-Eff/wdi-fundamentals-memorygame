3cdaaa8e21425b6a5d2dd145aa96cc1dc731bf2e
Fabricio <ravaneliferreira@hotmail.co.uk>
2017-01-09 21:09:29 +0000

    Added javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne == cardTwo) {
+	window.alert("You found a match!");
+} else {
+	window.alert("Not this time, bud.");
+}
